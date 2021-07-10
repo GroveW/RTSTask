cb8df2552ecfa89aface4893cc1d32e4c7d07070
GroveW <qswdw3ed3@gmail.com>
2021-07-10 20:57:56 +0200

    Add mouse clicks handling

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class Interactable : MonoBehaviour
+{
+    private List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+
+    public Color ColorApplyer;
+
+    [SerializeField]
+    private InputAction mouseHover;
+
+    private void Awake()
+    {
+        meshRenderers.AddRange(GetComponentsInChildren<MeshRenderer>());
+    }
+}
---
+++ Assets/Scripts/MouseInteractions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseInteractions : MonoBehaviour
+{
+    new private Camera camera;
+    
+    [SerializeField]
+    private LayerMask interactionsMask;
+
+    private Interactable currentlyInteracting = null;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        CheckRaycast();
+    }
+
+    void CheckRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out hit, 1000f, interactionsMask))
+        {
+            currentlyInteracting = hit.collider.GetComponent<Interactable>();
+        }
+        else if (currentlyInteracting)
+        {
+            currentlyInteracting = null;
+        }
+    }
+
+    public void HandleMouseClick(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            Debug.Log("Click");
+        }
+    }
+}
