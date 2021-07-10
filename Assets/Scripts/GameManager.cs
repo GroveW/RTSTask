31d6221f532452bd35bd8660c180e8d5a4f11417
GroveW <qswdw3ed3@gmail.com>
2021-07-10 16:36:30 +0200

    Add timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text currentTimeText;
+    private float currentTime = 0.0f;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(UpdateTimer), 0.0f, 1.0f);
+    }
+
+    private void UpdateTimer()
+    {
+        currentTime++;
+
+        currentTimeText.text = "Time: " + currentTime;
+    }
+}
