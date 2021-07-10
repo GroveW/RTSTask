3faaf74d2a681ad3e086944a65dd0216d8c82b40
GroveW <qswdw3ed3@gmail.com>
2021-07-10 16:29:41 +0200

    Add menu and starting game

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text bestTimeText;
+
+    void Start()
+    {
+        bestTimeText.text = "Best Time: " + PlayerPrefs.GetFloat("BestTime");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
