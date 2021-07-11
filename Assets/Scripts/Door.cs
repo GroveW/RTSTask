ef8cdd21aa7b4fd29e210ae8768b8823d2803f85
GroveW <qswdw3ed3@gmail.com>
2021-07-11 17:36:09 +0200

    Add door opening

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Door : Interactable
 {
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public override void Interact()
     {
         base.Interact();
+
+        anim.SetTrigger("Open");
     }
 
     public void MeetRestrictions()
